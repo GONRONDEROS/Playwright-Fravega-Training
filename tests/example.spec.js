25dc2f043ee4422b6117a1c4b61caa3766aba812
GONRONDEROS <gonzalorond@gmail.com>
2026-01-20 17:22:41 -0300

    Ejercitacion Finalizada

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import { ADDRGETNETWORKPARAMS } from 'node:dns';
 
 /**
  * @type {import("playwright-core").Page}
@@ -14,35 +15,66 @@ test.beforeAll(async({browser})=>{
 
 test('Verify placing and order', async () => {
   await page.goto("https://www.fravega.com/");
-  await page.locator("ul.dXuxpr > li.erbUBo > div.cbwmbG > a > span.brTaXr:has-text('Celulares')").click();
-  await page.locator("div.cgAxxT > label.kcAyqR:has-text('Samsung')").click();
-  await page.waitForURL("https://www.fravega.com/l/celulares/celulares-liberados/?marcas=samsung");
+  await Promise.all([
+    page.locator("ul.dXuxpr > li.erbUBo > div.cbwmbG > a > span.brTaXr:has-text('Celulares')").click(),
+    page.waitForURL("**/celulares/**")
+  ]);
+  await Promise.all([
+    page.locator("div.cgAxxT > label.kcAyqR:has-text('Samsung')").click(),
+    page.waitForURL("**/?marcas=samsung")  
+  ]);
   const checkboxCuotas = await page.locator("div.cFwhSX:has(div.fsNAZr:has-text('Cuotas sin interés')) a.hTZzZP > label.jIkFVx");
-  await checkboxCuotas.click();
+  await Promise.all([
+    checkboxCuotas.click(),
+    page.waitForURL(/formas-de-pago=12-cuotas-sin-interes/)
+  ]);
   expect(await checkboxCuotas.isChecked()).toBeTruthy();
   const checkboxApple = await page.locator("[id*='brand-filter-checkbox-apple-Apple']");
-  await checkboxApple.click();
-  await page.waitForURL(/apple/);
-  await page.locator("button.bVvgMq").click();
-  await page.locator("a.jVZMfP:has-text('Mayor descuento')").click();
-  await page.waitForURL(/HIGHEST_DISCOUNT/);
-  //await page.locator("ul.ebdNvu > li.kgGNyw").last().click();
-  await page.locator("ul.peNi").click();
-  const cartElement = await page.locator("h1[data-test-id='product-title']").first().textContent();
-  await page.getByRole('button', { name: 'Agregar al carrito' }).first().click();
-  await page.locator("button.user-cart-btn").click();
-  const cartElementToBuy = await page.locator("div.dPZWkG").first().textContent();
-  await expect(cartElement?.trim()).toBe(cartElementToBuy?.trim());
-  const elementsInCart = await page.locator("div.dMnKzX").count();
-  console.log(elementsInCart);
-  await page.pause();
+  await Promise.all([
+    checkboxApple.click(),
+    page.waitForURL(/apple/)
+  ]);
+  const orderButton = await page.locator("button.bVvgMq");
+  await orderButton.click();
+  await Promise.all([
+    page.locator("a.jVZMfP:has-text('Mayor descuento')").click(),
+    page.waitForURL(/HIGHEST_DISCOUNT/)
+  ]);
+  const selectableArticles = await page.locator("ul.peNi > li > article.bwMsmt");
+  await expect(selectableArticles.first()).toBeVisible();
+  const articleToSelect = await page.locator(".kUaLHc").first();
+  const nameOfArticleToSelect = await articleToSelect.textContent();
+  console.log("El producto a seleccionar es: " + nameOfArticleToSelect);
+  await Promise.all([
+    articleToSelect.click(),
+    page.waitForURL(/p/)
+  ]);
+  const selectedArticle = await page.locator("h1[data-test-id='product-title']").first().textContent();
+  console.log("El producto seleccionado es: " + selectedArticle);
+  if(selectedArticle?.trim() === nameOfArticleToSelect?.trim()){
+    await page.getByRole('button', { name: 'Agregar al carrito' }).first().click();
+    //await page.pause()
+  };
+  await page.waitForSelector(".Toastify__toast-body", { state: "visible" });
+  const nombreAGuardar = await page.locator("div.Toastify__toast-body > div > div.iGnvSC > div.llUDMb > p").first().textContent(); 
+  console.log("El producto en el pre carro es: " + nombreAGuardar);
+  const esteBotonDelCarrito = await page.locator("div.Toastify__toast-body >> text=Ver carrito");
+  await expect(esteBotonDelCarrito).toBeVisible();
+  await Promise.all([
+    esteBotonDelCarrito.click(),
+    page.waitForURL("**/chk-ui-headless")
+  ]);   
+  await page.waitForSelector("div.bqsZAS");
+  const elementsInCart = await page.locator("div.bqsZAS").count();
+  console.log("La cantidad de elementos en el carrito es: " + elementsInCart);
   for(let i = 0; i < elementsInCart; i++){
-    if(await page.locator("div.CEnZU").nth(i).textContent() === cartElementToBuy){
+    let elementInCartDescription = await page.locator("div.CEnZU").nth(i).textContent();
+    let elementCurated = elementInCartDescription?.trim()
+    console.log(elementCurated);
+    if(elementCurated === selectedArticle){
       await page.locator("#endPurchaseCart").click();
       break;
     }
   }
-  
-  await page.pause();
 });
 
